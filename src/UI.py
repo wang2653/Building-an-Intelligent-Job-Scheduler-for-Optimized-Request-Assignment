e3c74e64a3948eafdfd14ca2b3a1b97d77988a20
Ruoyu Li <lambert.li@mail.utoronto.ca>
2025-01-08 01:06:31 -0500

    Add page switches for about button and contact button

--- src/UI.py
+++ src/UI.py
@@ -19,6 +19,9 @@ with open("../assets/styles.css", "r") as css_file:
 def edit_image(image):
     return image"""
 
+# load the patient database
+df = pd.read_csv('../data/patientdata7.csv')
+
 # Basic test function for doctor interface
 def test_func(Patient_name, Arrival_time, Acuity_level, Treatment_plan):
     return ("Hello, " + Patient_name + ", you arrived at time " 
@@ -54,22 +57,29 @@ def login_with_google():
     else:
         return "Login failed. Please try again."
 
+# helper functions to open different pages
+def open_main_page():
+    return gr.update(visible=True), gr.update(visible=True), gr.update(visible=False), gr.update(visible=False)
 
-df = pd.read_csv('../data/patientdata7.csv')
+def open_about_page():
+    return gr.update(visible=False), gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)
+
+def open_contact_page():
+    return gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(visible=True)
 
 with gr.Blocks() as interface:
     gr.HTML(styles_css)  # Adding custom styles here
 
-    with gr.Row(equal_height=True, elem_id="header_row"):
+    with gr.Row(equal_height=True, visible=True, elem_id="header_row") as main_header:
         gr.Markdown("# Intelligent Scheduler for Emergence Department", elem_id="page_title")
-        gr.Button("Home", elem_id="home_btn", size="lg")
-        gr.Button("About", elem_id="about_btn", size="lg")
-        gr.Button("Contact", elem_id="contact_btn", size="lg")
+        home_btn = gr.Button("Home", elem_id="home_btn", size="lg")
+        about_btn = gr.Button("About", elem_id="about_btn", size="lg")
+        contact_btn = gr.Button("Contact", elem_id="contact_btn", size="lg")
         login_btn = gr.Button("Login", elem_id="login_btn", size="lg")
 
     login_btn.click(fn=login_with_google)
     
-    with gr.Column():
+    with gr.Column(visible=True) as main_display:
         with gr.Row():
             with gr.Column():
                 gr.Markdown('<h1 style="font-size: 4em;">Explore Better Decision Making</h1>', elem_id="main_text")
@@ -89,5 +99,22 @@ with gr.Blocks() as interface:
             )
 
             gr.Dataframe(value=df, label="Patient Data", elem_id="data_table")
+
+    # The about page
+    with gr.Column(visible=False) as about:
+        gr.Markdown("# This is the about page, waiting for more contents.  \n" +
+                    "For more info about our product, please see https://github.com/wang2653/Building-an-Intelligent-Job-Scheduler-for-Optimized-Request-Assignment.  \n" + 
+                    "For any thoughts or problems regarding to the web design, please contact liruoyu996@gmail.com, thank you.")
+    
+    # The contact page
+    with gr.Column(visible=False) as contact:
+        gr.Markdown("# This is the contact page, waiting for more contents.  \n" +
+                    "For more info about our product, please see https://github.com/wang2653/Building-an-Intelligent-Job-Scheduler-for-Optimized-Request-Assignment.  \n" + 
+                    "For any thoughts or problems regarding to the web design, please contact liruoyu996@gmail.com, thank you.")
+    
+    # Set different page visibilities for different pages
+    about_btn.click(fn=open_about_page, inputs=[], outputs=[main_header, main_display, about, contact])
+    home_btn.click(fn=open_main_page, inputs=[], outputs=[main_header, main_display, about, contact])
+    contact_btn.click(fn=open_contact_page, inputs=[], outputs=[main_header, main_display, about, contact])
 
 interface.launch()
